0bb90d48243771d03d4b7d412d3fb77a9de6498c
jeffmon <jeffmon@gmail.com>
2017-06-03 15:27:04 -1000

    finished ex

--- constructors.js
+++ constructors.js
@@ -18,11 +18,67 @@ function Spell(name, cost, description){
 }
 
 Spell.prototype.getDetails = function(){
-  return "Name = " + this.name + ". " + "Cost = " + this.cost + ". " + "Description = " + this.description + ".";
+  return this.name + "|" + this.cost + "|" + " " + this.description + ".";
 };
 
-var vanish = new Spell("Vanish", 5, "Immediately vanish from your surroundings");
+function DamageSpell(name, cost, damage, description){
+  this.damage = damage;
+  Spell.call(this, name, cost, description);
+}
 
+DamageSpell.prototype = Object.create(Spell.prototype, {
+  constructor: DamageSpell
+});
+
+function Spellcaster(name, health, mana){
+  this.name = name;
+  this.health = health;
+  this.mana = mana;
+  this.isAlive = true;
+}
+
+Spellcaster.prototype.inflictDamage = function(damage){
+  if(this !== undefined){
+    this.health -= damage;
+    if(this.health <= 0){
+      this.health = 0;
+      this.isAlive = false;
+    }
+  }
+
+};
+
+Spellcaster.prototype.spendMana = function(cost){
+  if(cost <= this.mana){
+    this.mana -= cost;
+    return true;
+  } else{
+    return false;
+  }
+};
+
+Spellcaster.prototype.invoke = function(whichSpell, target){
+  if(whichSpell instanceof Spell === false || whichSpell === null){
+    return false;
+  }
+  else{
+    if(this.mana >= whichSpell.cost && whichSpell instanceof DamageSpell){
+      if(target === null || target === undefined){
+        return false;
+      }else{
+        target.inflictDamage(whichSpell.damage);
+        this.spendMana(whichSpell.cost);
+        return true;
+      }
+
+    }else if(this.mana >= whichSpell.cost){
+      this.spendMana(whichSpell.cost);
+      return true;
+    }else if(this.mana < whichSpell.cost){
+      return false;
+    }
+  }
+};
 
   /**
    * Returns a string of all of the spell's details.
